d11ceb72d7f2a8009cd8c88602df2064f3f73843
xbexbex <xbexbex@helsinki.fi>
2017-08-03 13:20:07 +0300

    zydem

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,7 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
